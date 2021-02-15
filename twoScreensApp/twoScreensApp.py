0d5ac80e53b76a239a4c3482767b083b23354276
agsit <AvantGardeSIT@gmail.com>
2021-02-14 16:50:25 -0800

    re-org and addded a two screens template

--- twoScreensApp/twoScreensApp.py
+++ twoScreensApp/twoScreensApp.py
@@ -1,21 +1,24 @@
+# MIT License
+# Copyright (c) 2017-2021 Avant Garde Scientific and Industries Technologies [AGSIT]
+#
+# Description:
+#   This is the "main" module that contains the Kivy App and UI classes.
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.widget import Widget
-# Custom module for miscellenuous utility classes to support a GUI.
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+# Custom module for miscellaneous utility classes to support a GUI.
 from kvAPI.guiUtils import userPath
 
 Builder.load_file('kvSubPanels/filediag.kv')      # Layout file for GUI sub-panel
 Builder.load_file('kvSubPanels/statusbar.kv')     # Layout file for GUI sub-panel
 
 #======================================== APP class ===============================================#
-class onePanelApp(App):
+class twoScreensApp(App):
   """
     Python Kivy App class
 
-    The Kivy class is defined in the onePanel.kv Kivy file (NOTE: Kivy filename must be ALL lower
-    case and not include the "App" postfix). 
+    The Kivy class is defined in the twoScreensApp.kv Kivy file (NOTE: Kivy filename must be ALL lower case and not include the "App" postfix). 
 
     The Kivy "build" method is used here instead of the standard __init__ to define the various
     class properties
@@ -24,38 +27,64 @@ class onePanelApp(App):
     """
     This is the first method that Kivy calls to build the GUI.
     """
-    self.title = 'onePanel (ver0.0r210101)'
+    self.title = 'twoScreensApp (ver0.0r210101)'
     self.icon = 'agsitLogoV2_32x32.png'
     # This initialization file is used to keep the last file/folder the user has used so it can be
     # re-open easily the next time the user user wants to select a file.  It also saves the
     # location and size of the UI window when it was closed so the next time the UI is opened it
     # opens at the same location/size.
-    self.fileIO = userPath('onePanel.py')
-    # Instantantiate a OnePanelGui object for the GUI and at the same time also send the
-    # reference to this main app object to the onePanelui object so that properties from both
+    self.fileIO = userPath('twoScreensApp.py')
+    # Instantantiate a TwoScreensGui object for the GUI and at the same time also send the
+    # reference to this main app object to the TwoScreensGui object so that properties from both
     # classes can easily be access from each other.
     # NOTE: This will assign the GUI object to the Kivy "self.root" property, i.e., use
     #       "self.root" to access all Kivy layouts/widgets/classes.  It is the Kivy "root rule".
-    return OnePanelGui(self)
+    return TwoScreensGui(self)
+
+
+  def screenSelect(self, direction):
+    """
+      Method that calls Kivy (i.e., root property) to transition between the various screen
+    """
+    # Get all the available screen names
+    lstScreens = self.root.screen_names
+    screenIndex = lstScreens.index(self.root.current)
+    if direction == 'previous':
+      screenIndex -= 1
+      self.root.transition = SlideTransition(direction='right')
+      if screenIndex < 0:
+        # If the index is lower than zero, then go to the last screen
+        self.root.transition = SlideTransition(direction='left')
+        self.root.current = lstScreens[-1]
+        return
+    else:
+      screenIndex += 1
+      self.root.transition = SlideTransition(direction='left')
+      # If the index is greater than the number of screen, than move to the first one
+      if screenIndex >= len(lstScreens):
+        # If the index is greater than the number of available screen
+        self.root.transition = SlideTransition(direction='right')
+        self.root.current = lstScreens[0]
+        return
+    self.root.current = lstScreens[screenIndex]
 
 
   def selectFile(self):
     self.fileIO.fileType = [('text files', ('.txt', '.text')), ('all files', '.*')]
-    self.fileIO.pathSelect(pathTag='onePanel')
+    self.fileIO.pathSelect(pathTag='twoScreensApp')
     if self.fileIO.numPaths == 0: # User cancelled the selection
-      self.root.statusBar.lblStatusBar.text = ' User cancelled selection'
+      self.root.scrOne.statusBar.lblStatusBar.text = ' User cancelled selection'
     else: # The user selected an HDF5 file
-      self.root.statusBar.lblStatusBar.text = ' File loaded !'
+      self.root.scrOne.statusBar.lblStatusBar.text = ' File loaded !'
       # self.ui.ids.kvLytStatusBar.ids.kvLblStatusBar.text = ' File loaded !'
-      self.root.fileDiag.lblFilePath.text = self.fileIO.currentPaths[0]
-      self.root.fileDiag.selectButton.text = 'Selected File'
-
+      self.root.scrOne.fileDiag.lblFilePath.text = self.fileIO.currentPaths[0]
+      self.root.scrOne.fileDiag.selectButton.text = 'Selected File'
 
 #======================================== GUI class ===============================================#
-class OnePanelGui(GridLayout):
+class TwoScreensGui(ScreenManager):
   """
   This will instantiate the "root" Kivy property and the name used here MUST match exactly the Kivy
-  class name used in the onepanel.kv file and the name used in the build method of the app class
+  class name used in the twoScreensApp.kv file and the name used in the build method of the app class
   defined above.
   """
   def __init__(self, mainAppRef):
@@ -65,7 +94,7 @@ class OnePanelGui(GridLayout):
       Also pass as parameter the App object reference so that any App property or methods can
       easily be accessed in this class directly if needed.
     '''
-    GridLayout.__init__(self)    # instantiate the parent class
+    ScreenManager.__init__(self)    # instantiate the parent class
     self.app = mainAppRef        # Property to access the App properties easily, e.g., 
                                  # the initialization file where last window properties are saved.
     #------------------------------------ GUI Window properties -----------------------------------#
@@ -106,6 +135,13 @@ class OnePanelGui(GridLayout):
     self.app.fileIO.appsWriteDfltVal()
 
 
+class ScrOne(Screen):
+  pass
+
+class ScrTwo(Screen):
+  pass
+
+
 #========================================== GUI launch ============================================#
 if __name__ == "__main__":
-  onePanelApp().run()
+  twoScreensApp().run()
